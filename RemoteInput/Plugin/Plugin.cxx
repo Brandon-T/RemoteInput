077e18687638d6da824994f3e5de64964492bf21
Brandon-T <JustBrandonT@Gmail.com>
2020-03-07 16:06:32 -0500

    Update for Codeblocks

--- RemoteInput/Plugin/Plugin.cxx
+++ RemoteInput/Plugin/Plugin.cxx
@@ -223,7 +223,7 @@ void Reflect_Array_Index4D(EIOS* eios, jarray array, ReflectionArrayType type, s
 [[gnu::stdcall]] void __load()
 {
     printf("ATTACHED TO: %d\n", getpid());
-	
+
 	//std::thread not working well on Windows when called from DLL main and the library is freed immediately..
 	/*std::thread([&] {
 		auto reflector = std::unique_ptr<Reflection>(GetNativeReflector());
@@ -233,13 +233,13 @@ void Reflect_Array_Index4D(EIOS* eios, jarray array, ReflectionArrayType type, s
 			StartHook();
         }
 	}).detach();*/
-	
+
 	//Increase our reference count by 1..
 	//So that if someone calls `FreeLibrary` before the thread exists, we won't get a crash.
 	//Later on we will call `FreeLibraryAndExitThread`
 	HMODULE this_module = nullptr;
 	GetModuleHandleEx(GET_MODULE_HANDLE_EX_FLAG_FROM_ADDRESS, reinterpret_cast<LPCTSTR>(module), &this_module);
-	
+
 	HANDLE hThread = CreateThread(nullptr, 0, [](void* lpParam) __stdcall -> DWORD {
         auto reflector = std::unique_ptr<Reflection>(GetNativeReflector());
 		if (reflector)
@@ -247,11 +247,11 @@ void Reflect_Array_Index4D(EIOS* eios, jarray array, ReflectionArrayType type, s
 			control_center = std::make_unique<ControlCenter>(getpid(), false, std::move(reflector));
 			StartHook();
 		}
-		
-		
+
+
 		//Decrease our reference count by 1..
 		//So if `FreeLibrary` was called previous, our count reaches 0 and we'll be fred.
-		Free­Library­AndExit­Thread(module, 0);
+		FreeLibraryAndExitThread(module, 0);
 		return 0;
 	}, nullptr, 0, nullptr);
 	CloseHandle(hThread);
@@ -259,7 +259,6 @@ void Reflect_Array_Index4D(EIOS* eios, jarray array, ReflectionArrayType type, s
 
 [[gnu::stdcall]] void __unload()
 {
-	
     control_center.reset();
 }
 #elif defined(__APPLE__)
